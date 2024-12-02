9b2661c57a62c6432e8b5f6f5d25218bfee45083
RehanGith <chrehan1007@gmail.com>
2024-12-03 00:01:38 +0700

    Separeted UI data to viewModel class and Ui event in fragment

--- app/src/main/java/com/example/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/guesstheword/screens/game/GameFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.example.guesstheword.R
 import com.example.guesstheword.databinding.FragmentGameBinding
@@ -20,14 +19,9 @@ const val CUR_WORD = "current word"
 
 class GameFragment : Fragment() {
     // The current word
-    private var word = ""
+
+
     lateinit var viewModel: GameViewModel
-
-    // The current score
-    private var score:Int = 0
-
-    // The list of words - the front of the list is the next word to guess
-    private lateinit var wordList: MutableList<String>
 
     private lateinit var binding: FragmentGameBinding
 
@@ -44,89 +38,31 @@ class GameFragment : Fragment() {
         )
         Log.i("GameFragment", "ViewModelProvider called")
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
-        resetList()
-        nextWord()
-
-        binding.correctButton.setOnClickListener { onCorrect() }
-        binding.skipButton.setOnClickListener { onSkip() }
+        binding.correctButton.setOnClickListener {
+            viewModel.onCorrect()
+            updateWordText()
+            updateScoreText()
+        }
+        binding.skipButton.setOnClickListener {
+            viewModel.onSkip()
+            updateWordText()
+            updateScoreText()
+        }
         updateScoreText()
         updateWordText()
         return binding.root
     }
 
-    /**
-     * Resets the list of words and randomizes the order
-     */
-    private fun resetList() {
-        wordList = mutableListOf(
-            "queen",
-            "hospital",
-            "basketball",
-            "cat",
-            "change",
-            "snail",
-            "soup",
-            "calendar",
-            "sad",
-            "desk",
-            "guitar",
-            "home",
-            "railway",
-            "zebra",
-            "jelly",
-            "car",
-            "crow",
-            "trade",
-            "bag",
-            "roll",
-            "bubble"
-        )
-        wordList.shuffle()
-    }
-
-    /**
-     * Called when the game is finished
-     */
-    private fun gameFinished() {
+    fun gameFinished() {
         val bundle = Bundle()
-        bundle.putInt(SCORE, score)
+        bundle.putInt(SCORE, viewModel.score)
         findNavController().navigate(R.id.action_game_to_score, bundle)
     }
-    /**
-     * Moves to the next word in the list
-     */
-    private fun nextWord() {
-        //Select and remove a word from the list
-        if (wordList.isEmpty()) {
-            gameFinished()
-        } else {
-            word = wordList.removeAt(0)
-        }
-        updateWordText()
-        updateScoreText()
-    }
-
-    /** Methods for buttons presses **/
-
-    private fun onSkip() {
-        score--
-        nextWord()
-    }
-
-    private fun onCorrect() {
-        score++
-        nextWord()
-    }
-
-
-    /** Methods for updating the UI **/
-
-    private fun updateWordText() {
-        binding.wordText.text = word
+    fun updateWordText() {
+        binding.wordText.text = viewModel.word
 
     }
-
     private fun updateScoreText() {
-        binding.scoreText.text = score.toString()
+        binding.scoreText.text = viewModel.score.toString()
     }
 }
--- app/src/main/java/com/example/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/guesstheword/screens/game/GameViewModel.kt
@@ -1,13 +1,72 @@
 package com.example.guesstheword.screens.game
 
+import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.navigation.fragment.findNavController
+import com.example.guesstheword.R
 
 class GameViewModel: ViewModel() {
     init {
         Log.i("GameViewModel", "initialize GameViewModel")
+        resetList()
+        nextWord()
+
+    }
+    var word = ""
+    var score:Int = 0
+    lateinit var wordList: MutableList<String>
+
+    fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
     }
 
+    /**
+     * Called when the game is finished
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            //    gameFinished()
+        } else {
+            word = wordList.removeAt(0)
+        }
+    }
+    fun onSkip() {
+        score--
+        nextWord()
+    }
+
+    fun onCorrect() {
+        score++
+        nextWord()
+    }
+
+
+    /** Methods for updating the UI **/
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModel", "onCleared called")
