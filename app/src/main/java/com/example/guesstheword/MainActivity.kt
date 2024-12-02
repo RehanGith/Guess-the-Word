04380f2be3ae1ad2dbd06f855b9a041a5529c5d3
RehanGith <chrehan1007@gmail.com>
2024-12-02 19:10:54 +0700

    Created the navigation Bar

--- app/src/main/java/com/example/guesstheword/MainActivity.kt
+++ app/src/main/java/com/example/guesstheword/MainActivity.kt
@@ -3,25 +3,33 @@ package com.example.guesstheword
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import com.example.guesstheword.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var navController: NavController
+    lateinit private var navController: NavController
+    lateinit private var appBarConfiguration: AppBarConfiguration
+    lateinit private var drawerLayout: DrawerLayout
+    lateinit private var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        drawerLayout = binding.drawerlayout
         navController = this.findNavController(R.id.nav_host_fragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
-
-
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
     }
 }
